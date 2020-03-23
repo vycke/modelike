5627f1b5fb0153f73c86c396a9bf140011204106
Kevin Pennekamp <github@kevtiq.co>
2020-03-23 18:53:10 +0100

    forgot the index'

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import get from './get';
 import set from './set';
-import transform from './transform';
 import validate from './validate';
 import { schema, normalize, denormalize } from './normalize';
 
-export { get, set, transform, validate, normalize, denormalize, schema };
+export { get, set, validate, normalize, denormalize, schema };
