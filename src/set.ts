cd32535707e1fdd7557b5080e04720fbae0de824
Kevin Pennekamp <github@kevtiq.co>
2020-04-06 11:18:46 +0200

    small fix

--- src/set.ts
+++ src/set.ts
@@ -2,7 +2,7 @@ import { Primitive } from './types';
 // Sets the nested value based on tokenized string indicating the path of the
 // object. (e.g. set(obj, "user.info.address", "myValue")
 export default function(obj: object, path: string, val: Primitive): object {
-  if (!path || path === '') return obj;
+  if (path === '') return val as object;
   const tokens = path.split('.');
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
